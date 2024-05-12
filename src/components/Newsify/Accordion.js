c1d0dff658300a15a1473e047c60c51bdd6a1d73
Tanisha Chopra <tanishac768@gmail.com>
2024-05-12 14:28:11 +0530

    compeleted

--- src/components/Newsify/Accordion.js
+++ src/components/Newsify/Accordion.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Accordion.css'
 import add from '../../Image/add-btn.svg'
 
 const Accordion = () => {
+    const [selected, setSelected] = useState({})
+
     const quesAsked = [{
         question: "What is Team Newsify?",
         content: "It is alomost completely automated as a system . You can schedule plan and create calenders for your upcoming males. You can Anylzer Ai, tracker , Ai reporter, Ai to create a smooth business plan for your and your teamateswith one click!"
@@ -31,26 +33,39 @@ const Accordion = () => {
         question: "Can i get a live demo of Newsify in action?",
         content: "It is alomost completely automated as a system . You can schedule plan and create calenders for your upcoming males. You can Anylzer Ai, tracker , Ai reporter, Ai to create a smooth business plan for your and your teamateswith one click!"
     }]
-  return (
-    <div>          <div className='newsify-questions-asked'>
-    <div className='newsify-questions-text-box'>
-        <div className='newsify-questions-heading'>Freequently asked questions</div>
-        <div className='newsify-questions-subtext'>We`re happy to answer your questions</div>
-    </div>
-    <div className='accordion-section'>
-        {
-            quesAsked.map((accordion, index) => {
-                return (<div key={index} className='accordion-data'>
-                    <div className='accordion-card'>
-                        <div className='accordion-question'>{accordion.question}</div>
-                        <div className='accordion-content'>{accordion.content}</div>
-                    </div>
-                    <button className='add-btn'><img src={add}></img></button>
-                </div>)
-            })
-        }</div>
-</div></div>
-  )
+
+    function onClickDetails(index) {
+        if (selected == index) {
+            return setSelected(null)
+        }
+        setSelected(index)
+    }
+
+return (
+    <div>
+        <div className='newsify-questions-asked'>
+            <div className='newsify-questions-text-box'>
+                <div className='newsify-questions-heading'>Freequently asked questions</div>
+                <div className='newsify-questions-subtext'>We`re happy to answer your questions</div>
+            </div>
+            <div className='accordion-section'>
+                {
+                    quesAsked.map((accordion, index) => {
+                        return (<div key={index} className='accordion-data'>
+                            <div className='accordion-card'>
+                                <div className='accordion-question'>{accordion.question}
+                                    <button className='add-btn' onClick={() => onClickDetails(index)}><img src={add}></img></button>
+                                </div>
+
+                                <div className={selected == index ? 'accordion-content show' : 'accordion-content'}>{accordion.content}</div>
+                            </div>
+
+                        </div>)
+                    })
+                }
+            </div>
+        </div></div>
+)
 }
 
 export default Accordion
